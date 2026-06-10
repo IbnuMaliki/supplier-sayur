28503f86500ed0026eaad2097991a0b76f5f16da
IbnuMaliki <ibnu.maliki1705@gmail.com>
2026-06-10 14:20:22 +0700

    Add files via upload

---
+++ router.php
@@ -0,0 +1,10 @@
+<?php
+$uri = urldecode(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+$file = __DIR__ . $uri;
+
+if ($uri !== '/' && file_exists($file) && !is_dir($file)) {
+    return false;
+}
+
+$index = __DIR__ . '/index.php';
+require $index;
