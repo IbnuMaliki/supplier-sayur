e1cbe8084979408a1228807e1766f572e86579af
IbnuMaliki <ibnu.maliki1705@gmail.com>
2026-06-10 14:22:22 +0700

    Update router.php

--- router.php
+++ router.php
@@ -2,9 +2,8 @@
 $uri = urldecode(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
 $file = __DIR__ . $uri;
 
-if ($uri !== '/' && file_exists($file) && !is_dir($file)) {
-    return false;
+if (file_exists($file) && !is_dir($file)) {
+    return false; // serve file statik langsung
 }
 
-$index = __DIR__ . '/index.php';
-require $index;
+require __DIR__ . '/index.php';
